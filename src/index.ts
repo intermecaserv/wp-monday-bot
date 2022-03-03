e2cd44aecdf67020f6b2920b0a98635405e7a3ad
Alin Florin <alin@qoffice.ro>
2022-03-04 00:39:38 +0200

    d

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,30 @@
 import { config } from 'dotenv';
+import { chromium } from 'playwright';
+import { NODE_ENV, SLOWNESS, USER_DATA_DIR } from './config';
 
 config({
-    debug: process.env.NODE_ENV === 'dev'
+    debug: NODE_ENV === 'dev'
 });
-console.log(process.env.NODE_ENV);
+
+(async () => {
+    const browser = await chromium.launchPersistentContext(USER_DATA_DIR, {
+        headless: NODE_ENV !== 'dev',
+        slowMo: SLOWNESS
+    });
+    const wpPage = browser.pages().length > 0 ? browser.pages()[0] : (await browser.newPage());
+    await wpPage.goto('https://web.whatsapp.com', {
+        referer: 'https://google.com'
+    });
+
+    const qrLocator = wpPage.locator('canvas[role=img]');
+    if (qrLocator != null) {
+        const qrImageBytes = await qrLocator.evaluate(x => {
+            const canvas = x as HTMLCanvasElement;
+            const image = canvas.getContext('2d').getImageData(0, 0, canvas.clientWidth, canvas.clientHeight);
+            return image.data;
+        });
+        
+    } else {
+        
+    }
+})();
