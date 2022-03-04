c253a9444fcbcc6e20b50a57aadf420ebc314178
Alin Florin <alin@qoffice.ro>
2022-03-04 02:17:04 +0200

    f

--- src/index.ts
+++ src/index.ts
@@ -8,60 +8,55 @@ config({
     debug: NODE_ENV === 'dev'
 });
 
-let qrImageBytes: Blob | undefined;
+let qrImage: string | undefined;
 let readQrInterval: NodeJS.Timeout | undefined;
 
 const app = express();
 
 app.get('/', async (_, res) => {
     res
-        .header('Content-Disposition', 'attachment; filename=qr.png')
-        .header('Content-Length', qrImageBytes == null ? '0' : qrImageBytes.size.toString())
-        .send(qrImageBytes);
+        .header('Content-Type', 'text/html')
+        .send(`
+        <img src="${qrImage}" />
+        `);
 });
 
 app.listen(PORT, () => {
     console.log('API listening...');
-    (async () => {
-        const browser = await chromium.launchPersistentContext(USER_DATA_DIR, {
-            headless: NODE_ENV !== 'dev',
-            slowMo: SLOWNESS
-        });
-        const wpPage = browser.pages().length > 0 ? browser.pages()[0] : (await browser.newPage());
-        await wpPage.goto('https://web.whatsapp.com', {
-            referer: 'https://google.com'
-        });
-
-        const qrLocator = wpPage.locator('canvas[role=img]');
-
-        const uriToBlob = (dataURI: string) => {
-            var byteString = Buffer.from(dataURI.split(',')[1], 'base64');
-            return new Blob([byteString]);
-        }
-
-        const readQrImage = async () => {
-            qrImageBytes = await qrLocator.evaluate(x => {
-                const canvas = x as HTMLCanvasElement;
-                const dataUrl = canvas.toDataURL("image/png");
-                return uriToBlob(dataUrl);
-            });
-            const qrLocatorTest = wpPage.locator('canvas[role=img]');
-            if (qrLocatorTest == null && readQrInterval != null) {
-                clearInterval(readQrInterval);
-            }
-        };
-
-        if (qrLocator != null) {
-            await readQrImage();
-            readQrInterval = setInterval(() => {
-                (async () => {
-                    await readQrImage();
-                })();
-            }, 5000);
-
-        } else {
-
-        }
-    })();
 });
 
+(async () => {
+    const browser = await chromium.launchPersistentContext(USER_DATA_DIR, {
+        headless: NODE_ENV !== 'dev',
+        slowMo: SLOWNESS
+    });
+    const wpPage = browser.pages().length > 0 ? browser.pages()[0] : (await browser.newPage());
+    await wpPage.goto('https://web.whatsapp.com', {
+        referer: 'https://google.com'
+    });
+
+    const qrLocator = wpPage.locator('canvas[role=img]');
+
+    const readQrImage = async () => {
+        qrImage = await qrLocator.evaluate(x => {
+            const canvas = x as HTMLCanvasElement;
+            return canvas.toDataURL("image/png");
+        });
+        const qrLocatorTest = wpPage.locator('canvas[role=img]');
+        if (qrLocatorTest == null && readQrInterval != null) {
+            clearInterval(readQrInterval);
+        }
+    };
+
+    if (qrLocator != null) {
+        await readQrImage();
+        readQrInterval = setInterval(() => {
+            (async () => {
+                await readQrImage();
+            })();
+        }, 5000);
+
+    } else {
+
+    }
+})();
