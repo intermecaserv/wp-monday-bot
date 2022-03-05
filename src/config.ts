15ccdb6ec5b794c4927f5352d258f2a61d35abcb
Alin Florin <alin@qoffice.ro>
2022-03-05 18:18:58 +0200

    d

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
 export const USER_DATA_DIR = process.env.USER_DATA_DIR || './data';
 export const NODE_ENV = process.env.NODE_ENV || "dev";
 export const SLOWNESS = process.env.SLOWNESS ? +process.env.SLOWNESS : 50;
+export const GROUP_NAME = process.env.GROUP_NAME || "Test Group";
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { config } from 'dotenv';
 import { chromium } from 'playwright';
 import express from 'express';
-import { NODE_ENV, SLOWNESS, USER_DATA_DIR } from './config';
+import { GROUP_NAME, NODE_ENV, SLOWNESS, USER_DATA_DIR } from './config';
 
 
 config({
@@ -9,7 +9,8 @@ config({
 });
 
 let qrImage: string | undefined;
-let readQrInterval: NodeJS.Timeout | undefined;
+let readQrInterval: any | undefined;
+let readMessagesTimeout: any | undefined;
 
 const app = express();
 
@@ -32,39 +33,86 @@ app.listen(3000, () => {
     });
     const wpPage = browser.pages().length > 0 ? browser.pages()[0] : (await browser.newPage());
     await wpPage.goto('https://web.whatsapp.com', {
-        referer: 'https://contacts.google.com'
+        referer: 'https://google.com'
     });
 
     const qrLocator = wpPage.locator('canvas[role=img]');
 
+    const processMessages = async () => {
+        console.log('Doing...');
+    }
+
+    const startProcessingIncomingMessages = async () => {
+        console.log('Processing messages...');
+        await processMessages();
+        readMessagesTimeout = setTimeout(() => {
+            (async () => {
+                await startProcessingIncomingMessages();
+            })();
+        }, 5000);
+    }
+
     const startBot = async () => {
-        console.log('Bot started');
+        // find group tab
+        console.log('Trying to find the group tab...');
+        const allGroupsLocator = wpPage.locator(`div[aria-label='Chat list']`);
+        await allGroupsLocator.waitFor({ state: 'visible', timeout: 10000 });
+        const groupTabLocator = allGroupsLocator.locator(`span`, {
+            hasText: GROUP_NAME
+        });
+        await groupTabLocator.waitFor({ state: 'visible', timeout: 10000 });
+        groupTabLocator.click();
+        console.log('Clicked group tab.');
+        console.log('Scanning all incoming messages...');
+        await startProcessingIncomingMessages();
     }
 
     const readQrImage = async () => {
-        const qrLocatorTest = wpPage.locator('canvas[role=img]');
-        if (qrLocatorTest == null && readQrInterval != null) {
+        const qrLocatorTest = await wpPage.$$('canvas[role=img]');
+        if (!qrLocatorTest.length && readQrInterval != null) {
+            console.log('QR scanned successfully. Starting bot.');
             clearInterval(readQrInterval);
-            await startBot();
+            try {
+                await startBot();
+            } catch (err3) {
+                console.error(err3);
+                process.exit(1);
+            } 
             return;
         }
-        qrImage = await qrLocator.evaluate(x => {
+        qrImage = await qrLocatorTest[0].evaluate(x => {
             const canvas = x as HTMLCanvasElement;
             return canvas.toDataURL("image/png");
         });
-        
     };
 
-    if (qrLocator != null) {
+
+    try {
+        console.log('Scanning for QR...');
+        await qrLocator.waitFor({ state: 'visible', timeout: 10000 });
+        console.log('QR is visible, starting reading it every 5 sec.');
         await readQrImage();
         readQrInterval = setInterval(() => {
             (async () => {
+                console.log('Reading QR...');
                 await readQrImage();
             })();
         }, 5000);
-
-    } else {
-        await startBot();
+    } catch (err) {
+        console.log('QR not present, are we already logged in?');
+        const searchBarLocator = wpPage.locator('text=Search or start new chat');
+        try {
+            await searchBarLocator.waitFor({ state: "visible", timeout: 10000 });
+            console.log('We are logged in.');
+            try {
+                await startBot();
+            } catch (err3) {
+                console.error(err3);
+                process.exit(1);
+            } 
+        } catch (err2) {
+            console.error('We are not logged in. Error.');
+            throw new Error('Login failed.');
+        }
     }
-
 })();
